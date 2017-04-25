88058d1b2333eb183f330274ffc0112a3855b62a
Rushikesh Naiknaware <rushikesh.naiknaware@atmecs.com>
2017-04-25 14:30:01 +0530

    Resolve NoClassDefFoundError by putting commons-lang3 jar into another jar

--- src/main/java/com/example/Greeting.java
+++ src/main/java/com/example/Greeting.java
@@ -1,11 +1,38 @@
 package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 public class Greeting {
+
+	public int count_words(String line) {
+			String[] words =	StringUtils.split(line,' ');
+			return (words == null)? 0 : words.length;
+	}
+
+
+	public void greet(){
+		List<String> lst = new ArrayList<String>();
+		
+		lst.add("Hello ");
+		lst.add("Maven");
+		
+		for(String ele : lst){
+			System.out.println("Greeting "+ele);
+		}
+		
+		System.out.println("-----------------------------");
+	}
 
 	public static void main(String[] args) {
 			System.out.println("-------------------------------------");
 			System.out.println("Greeting card");
 			System.out.println("Good Morning");
 			System.out.println("-------------------------------------");
+			Greeting greets = new Greeting();
+			greets.greet();
+			
+			System.out.println("count : "+greets.count_words("Hello ru;shi how are you"));
 			
 
 	}

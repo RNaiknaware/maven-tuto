7a4d84c6f9e46c2cc2740d591638ce6f8920c931
Rushikesh Naiknaware <rushikesh.naiknaware@atmecs.com>
2017-04-25 11:01:40 +0530

    Adding pom.xm and dummy files to keep folder in place

---
+++ src/main/java/com/example/Greeting.java
@@ -0,0 +1,13 @@
+package com.example;
+public class Greeting {
+
+	public static void main(String[] args) {
+			System.out.println("-------------------------------------");
+			System.out.println("Greeting card");
+			System.out.println("Good Morning");
+			System.out.println("-------------------------------------");
+			
+
+	}
+
+}
